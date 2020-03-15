e6b2c71fe5cafd28319ef746019f8e2708be1dc3
David Marshall <dpegasusm@gmail.com>
2020-03-15 15:53:43 -0400

    Trail roles first pass

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord=require('discord.js');
 const bot=new Discord.Client();
-// const mysql = require('mysql');
+const mysql = require('mysql');
 const ontime = require('ontime');
 const config=require('./config.json');
                     
@@ -10,10 +10,103 @@ bot.on('ready',  async () => {
     console.log( "Bot Ready for action." );
 });
 
+/** 
+CREATE TABLE `free_trial` (
+  `discord_id` smallint(6) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  PRIMARY KEY (discord_id)
+);
+*/
+
+async function GetFreeTrial(database, discord_id)
+{
+    return await new Promise(function(resolve) {
+        let connection = mysql.createConnection(database);
+    
+        connection.connect(async function(error) {
+            if(error)
+            {
+                console.log("Error connecting to SQL: "+error.stack);
+                    connection.end(function(err) {
+                        
+                    });
+                return resolve(false);
+    
+            }
+    
+            let date = new Date();
+            let sqlQuery = "SELECT * FROM free_trial WHERE discord_id = '"+discord_id+"';";
+
+            connection.query( sqlQuery, async function(error, results) {
+                if(error) { throw error; }
+                        
+                for(let i = 0; i < results.length; i++)
+                {
+                    let currentUser = results[i];
+
+                    let dateTimeParts = currentUser.end_date.split(/[- :]/); // regular expression split that creates array with: year, month, day, hour, minutes, seconds values
+                    dateTimeParts[1]--; // monthIndex begins with 0 for January and ends with 11 for December so we need to decrement by one
+
+                    let dateObject = new Date(...dateTimeParts);
+                            
+                    if( dateObject.getTime() <= date.getTime() )
+                    {		
+                        return resolve(false);
+                    }
+                }
+                
+            });
+    
+            connection.end();
+    
+            return resolve(true);
+        });
+    });
+}
+
+async function SetFreeTrial(database, discord_id)
+{
+    return await new Promise(function(resolve) {
+        let connection = mysql.createConnection(database);
+    
+        connection.connect(async function(error) {
+            if(error)
+            {
+                console.log("Error connecting to SQL: "+error.stack);
+                    connection.end(function(err) {
+                        
+                    });
+                return resolve(false);
+    
+            }
+    
+            let sqlQuery = "INSERT INTO free_trial (discord_id,start_date,end_date) VALUES ('"+discord_id+"',NOW(), NOW() + INTERVAL 1 DAY );";
+
+			connection.query(sqlQuery, async function(error, results) {
+				if(error) { throw error; }
+				return resolve(true);
+			});
+    
+            connection.end();
+    
+            return resolve(false);
+        });
+    });
+}
+
 ontime({
 	cycle:[config.remindTime]
 }, async function(ot) {
 	await outputDailyReminders( config );
+	return ot.done();
+});
+
+// remove the free trial at the remove time.
+ontime({
+	cycle:[config.trialRemoveTime]
+}, async function(ot) {
+	await RemoveFreeTrials( config );
 	return ot.done();
 });
 
@@ -35,10 +128,32 @@ bot.on( 'guildMemberUpdate', ( oldMember, newMember ) => {
     const rotomSupport = bot.channels.get(config.rotomSupportChannel);
     const rotomChannel = bot.channels.get(config.rotomChannel);
     const rotomPlusChannel = bot.channels.get(config.rotomPlusChannel);
+    let addTrial = false;
 
     if( newMember.roles.has( config.verifiedRole ) && ! oldMember.roles.has( config.verifiedRole ) ) {
         upgradeChannel.send( newMember + " you've been verified, please type \"`upgrade`\" in this channel to view the subscription options. If you have questions about the subscription process please check out " + upgradeRules + " Thank you so much for the support!" );
         logChannel.send( `${newMember} was verified.` );
+        addTrial = true;
+    }
+
+    if( ( newMember.roles.has( config.trialRole ) && ! oldMember.roles.has( config.verifiedRole ) ) || addTrial === true ) {
+        // add free trial role if database says its not already there
+        trialEligible = GetFreeTrial( config.sqlConnection, newMember );
+
+        if ( trialEligible === true ) {
+            // remove trial
+            newMember.addRole(config.trialRole);
+            SetFreeTrial();
+
+            // send removal notice
+            bot.channels.get( config.welcomeChannel ).send( 'Trial added' );
+        } else {
+            // remove trial
+            newMember.removeRole(config.trialRole);
+
+            // send removal notice
+            bot.channels.get( config.welcomeChannel ).send( 'Trial Removed' );
+        }
     }
 
     if( newMember.roles.has( config.collectorRole ) && ! oldMember.roles.has( config.collectorRole ) ) {
@@ -51,6 +166,36 @@ bot.on( 'guildMemberUpdate', ( oldMember, newMember ) => {
         logChannel.send( `${newMember} was upgraded to Ace Trainer.` );
     }
 });
+
+// Remove the free trials from members whose time has expired.
+async function RemoveFreeTrials( config ) {
+	return await new Promise( function( resolve ) {
+
+        let trialMembers = bot.guilds.get( config.guild ).members.filter(member => { 
+            if ( member.roles.has( config.trialRole ) ) {
+                return false;
+            } else {
+                return true;
+            }
+        }).map(member => {
+            return member;
+        });
+
+        for ( const member of trialMembers ) {
+            trialEligible = GetFreeTrial( config.sqlConnection, member );
+
+            if ( trialEligible === false ) {
+                // remove trial
+                member.removeRole(config.trialRole);
+
+                // send removal notice
+                bot.channels.get( config.welcomeChannel ).send( randomMessage );
+            }
+        }
+
+        return resolve(true);
+	});
+}
 
 async function outputDailyReminders( config ) {
 	return await new Promise( function( resolve ) {
@@ -85,3 +230,73 @@ function getReminder( member ) {
     
     return reminders[ Math.floor( Math.random()*reminders.length ) ];
 }
+
+// allow a message responder
+bot.on("message", (message) => {
+    // Messages need to be prefixed.
+    if (!message.content.startsWith( config.prefix ) || message.author.bot) return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    const memberID = message.sender;
+
+    switch (command) {
+        case "rotomhelp" :
+            message.channel.send({embed: {
+                color: 3447003,
+                author: {
+                name: 'Rotom',
+                icon_url: "https://cdn.discordapp.com/avatars/605111494408929280/3589dadf593215dca78b05ea2942dc96.png?size=256"
+                },
+                title: "Rotom Support Commands",
+                description: `**!help**
+                !help sends instructions on some basic commands 
+                
+                **!rotom**
+                !rotom is the command to register for alarms.
+                This is available in a designated channel set by the admin.
+                Once you successfully register, the bot will send you a DM with a greeting message
+                
+                **!stop**
+                !stop Temporarily stops alarms
+                
+                **!start**
+                !start Restarts alarms if they were stopped
+                
+                **!unregister**
+                !unregister Deletes all users tracking information. After !unregistering, you would need to register again with !rotom before new alarms.
+                
+                **!location**
+                !location Tallinn kesklinn for example, would register a users's location to 59.42685179999999,24.7595564
+                Instead of "Tallinn kesklinn" you can search for any address, city, country or known area available in google or specify a Latitude and Longitude.
+                
+                **!area add**
+                !area add Tallinn If the admin has set up a Geofence name Tallinn, this command will add tracking from that area to the user.
+                In case the area is not configured, the bot will reply with the areas that are available.
+                You can also use multiple areas in the same command (separated by spaces) and only available ones will be added.
+                
+                **!area remove**
+                !area remove Tallinn Removes Tallinn from tracked areas if configured and currently tracked. You can also use multiple areas in the same command (separated by spaces) and only available ones will be removed.
+                
+                **!area list**
+                !area list lists the possible areas you can add.
+                
+                **!tracked**
+                !tracked Shows a details list of Monsters, Raids, Eggs and quests user is tracking. Should the message exceed 6000 charecters, it will be uploaded into hastebin and a link will be sent to user. If hastebin is down (like it often is), the bot will create a temporary file and upload it to discord as a reply.
+                
+                See #rotom-support for more.`,
+                timestamp: new Date(),
+                footer: {
+                text: "Boston Pogo"
+                }
+              }
+            });
+            break;
+        
+        case "freetrial" :
+            memberID.addRole(config.trialRole);
+
+            break;
+    }
+});
